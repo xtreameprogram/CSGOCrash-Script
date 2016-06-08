41da15c876fa7490ddd3f8d9fbeaca78c7e56782
xtreameprogram <fordsmith18@yahoo.com>
2016-06-07 21:23:43 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+var baseBet = 100;
+var maxBet = 200;
+var cashOut = 2;
+var onLossIncreaseQty = 2;
+var maxLoseTrain = 2;
+// YOU CAN SCREW THIS UP IF YOU TOUCH BELOW. IF YOU DO, I AM NOT RESPONSIBLE
+var currentBet = baseBet;
+window.bet = true;
+var loseTrain = [];
+
+engine.on('game_starting', function() {
+        var lastGamePlay = engine.lastGamePlay();
+
+        if (lastGamePlay == 'LOST') {
+            currentBet = currentBet * onLossIncreaseQty;
+
+        } else if (lastGamePlay == 'WON') {
+            currentBet = baseBet;
+        }
+
+        if (currentBet > maxBet) {
+            currentBet = baseBet;
+        }
+        if (window.bet) {
+            engine.placeBet(currentBet * 100, Math.round(cashOut * 100), false);
+            console.log('Bet placed');
+        }
+});
+
+engine.on('game_crash', function(data) {
+    if (data.game_crash / 100 >= 2) {
+        loseTrain = [];
+        window.bet = true;
+        console.log('Last was win, resetting settings');
+    } else {
+        loseTrain.push('LOSE');
+        console.log('Loss');
+    }
+    if (loseTrain.length > maxLoseTrain - 1) {
+        window.bet = false;
+        console.log('Lose Train Exceeded');
+    }
+});
